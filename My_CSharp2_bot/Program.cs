9d9062db669f407e4c0d81c0477b2e2576061101
maxim10593 <maximdev10593@gmail.com>
2023-09-16 12:14:03 +0300

    Добавьте файлы проекта.

---
+++ My_CSharp2_bot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace My_CSharp2_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
