d2a88391bd87102438b99aafaf8dff076fccbf5c
maxim10593 <maximdev10593@gmail.com>
2023-09-16 12:41:06 +0300

    Добавленна поддержка Telegram bot API

--- My_CSharp2_bot/Program.cs
+++ My_CSharp2_bot/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace My_CSharp2_bot
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace My_CSharp2_bot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var botClient = new TelegramBotClient("6678742161:AAH2-0RGOa3H4M7IpNKiSdXAJITqPLHVi2U");
+
+            using CancellationTokenSource cancellationTokenSource = new();
+
+            botClient.StartReceiving(
+                updateHandler: UpdateHandlerAsync,
+                pollingErrorHandler: PollingErrorHandler,
+                cancellationToken: cancellationTokenSource.Token
+                );
+        }
+
+        private static async Task UpdateHandlerAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+
+        }
+
+        private static async Task PollingErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+
         }
     }
 }
