4ba6d0149a4e6cf17ac7291c4d8a850aff3ca863
maxim10593 <maximdev10593@gmail.com>
2023-09-26 23:30:00 +0300

    Добавлен абстрактний модуль 1 уровня : Диалоговое состояние. Взаимодействующий с машиной состояний (модуль 2 уровня) которая скоро будет добавленная.

---
+++ My_CSharp2_bot/Architecture/Abstract/DialogStateBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_CSharp2_bot.Architecture.Abstract
+{
+    internal abstract class DialogStateBase
+    {
+        protected readonly DialogStateMachine stateMachine;
+
+        public DialogStateBase(DialogStateMachine stateMachine)
+        {
+            this.stateMachine = stateMachine;
+        }
+    }
+}
