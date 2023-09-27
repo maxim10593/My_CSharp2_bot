65b0edf8477a54a56e074af8cbe564df77f099d6
maxim10593 <maximdev10593@gmail.com>
2023-09-27 04:15:48 +0300

    Добавленна абстракция системы диалоговых сущностей (модуль 4 уровня)

---
+++ My_CSharp2_bot/Architecture/Abstract/DialogEntitySystemBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_CSharp2_bot.Architecture.Abstract
+{
+    internal abstract class DialogEntitySystemBase<TEntityId, TDialogStateMachineKey>
+        where TEntityId : notnull
+        where TDialogStateMachineKey : notnull
+    {
+        protected Dictionary<TEntityId, IDialogEnity<TEntityId, TDialogStateMachineKey>> entityMap = new();
+
+        public void AddEntity(IDialogEnity<TEntityId, TDialogStateMachineKey> enity)
+        {
+            this.entityMap.Add(enity.Id, enity);
+        }
+
+        public bool ContainsEntity(TEntityId entityId)
+        {
+            return this.entityMap.ContainsKey(entityId);
+        }
+
+        public void RemoveEntity(TEntityId entityId)
+        {
+            this.entityMap.Remove(entityId);
+        }
+    }
+}
